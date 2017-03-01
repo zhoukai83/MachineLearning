4f0ca0dfbd526080039af9c7e8350273eefb66ca
Kai Zhou <zhoukai83@gmail.com>
2017-03-01 22:00:28 +0800

    1. More flexble way to label feature
    2. Feature Enginee
    
      a. exact more feature from existing feature
    
      b.when some value count is small,  put them together
    
    3. Evaluate bunch of estimators

--- Titanic/titanic2.py
+++ Titanic/titanic2.py
@@ -1,29 +1,27 @@
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import AdaBoostClassifier
 
-from sklearn.ensemble import IsolationForest
 from sklearn.feature_selection import SelectKBest
-from sklearn.ensemble import ExtraTreesClassifier
+
 from sklearn import metrics
 from sklearn import cross_validation
+from sklearn import ensemble
+
+
+from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.neighbors import KNeighborsClassifier
+
 
 try:
     from sklearn.model_selection import train_test_split
-    from sklearn.model_selection import KFold
 except:
     from sklearn.cross_validation import train_test_split
-    from sklearn.cross_validation import KFold
 
-from sklearn.preprocessing import Imputer
 
 import pandas as pd
-
-import csv
-import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -52,52 +50,120 @@ def __scorer(estimator, X, y):
     return metrics.accuracy_score(y, Y)
 
 
+import re
+def get_number(x):
+    match = re.search(r"\d\d+", x, re.IGNORECASE)
+    if match:
+        result = match.group()
+    else:
+        result = "0"
+    return int(result)
+
+
+def pre_process_data(origin_data):
+    data = origin_data.groupby(origin_data["Pclass"]).apply(lambda d: d.fillna(d["Fare"].mean()))
+
+    data["FirstName"] = data.Name.apply(lambda n: n.split(",")[0].strip())
+    data["Temp"] = data.Name.apply(lambda n: n.split(",")[1])
+    data["Title"] = data.Temp.apply(lambda n: n.split(".")[0].strip())
+
+    data['Title'] = data['Title'].replace(
+        ['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona', 'the Countess'],
+        'Other')
+    data['Title'] = data['Title'].replace('Mlle', 'Miss')
+    data['Title'] = data['Title'].replace('Ms', 'Miss')
+    data['Title'] = data['Title'].replace('Mme', 'Mrs')
+
+    data["FarmilyName"] = data.Temp.apply(lambda n: n.split(".")[1].strip())
+
+    data["FarmilySize"] = data["Parch"] + data["SibSp"]
+
+    data['CabinLetter'] = data['Cabin'].apply(lambda x: str(x)[0])
+    data["CabinNumber"] = data['Cabin'].apply(lambda x: get_number(str(x)))
+
+    data["Ticket"] = data['Ticket'].apply(lambda x: get_number(str(x)))
+
+    data["Embarked"] = data["Embarked"].factorize()[0]
+
+    data.loc[data['FarmilySize'] >= 4, 'FarmilySize'] = 4
+
+    #
+    data = data.drop(["Cabin"], axis=1)
+    data = data.drop("Temp", axis=1)
+    # data = data.drop(["Name", "SibSp", "Parch", "FarmilyName", "FirstName", "CabinNumber"], axis=1)
+    data = data.drop(["Name", "SibSp", "Parch", "FarmilyName", "FirstName"], axis=1)
+    return data
+
+
 def predict():
     origin_data = pd.read_csv("train.csv")
-    train_data = origin_data.drop(["Cabin"], axis=1)
+    print("origin_data shape:", origin_data.shape)
+    train_data = pre_process_data(origin_data)
     train_data = train_data.dropna()
     train_result = train_data.Survived
-    train_data = train_data.drop(["Survived"], axis=1)
+    train_data = train_data.drop(["Survived", "PassengerId"], axis=1)
 
     test_data = pd.read_csv("test.csv")
+    test_data = pre_process_data(test_data)
     test_data["Age"].fillna(test_data["Age"].mean(), inplace=True)
-    test_data = test_data.groupby(test_data["Pclass"]).apply(lambda d: d.fillna(d["Fare"].mean()))
-    test_data = test_data.drop(["Cabin"], axis=1)
+    test_data = test_data.drop(["PassengerId"], axis=1)
 
-    encode_label_index = ["Sex", "Embarked", "Ticket", "Name"]
-    for column in encode_label_index:
+    print("shape:", train_data.shape, test_data.shape)
+    print("column", train_data.columns)
+
+    for y in train_data.columns:
+        if train_data[y].dtype != "object":
+            continue
+
+        print(y, train_data[y].dtype)
+
         label_encoder = preprocessing.LabelEncoder()
-        column = train_data.columns.get_loc(column)
+        column = train_data.columns.get_loc(y)
 
         label_transform_data = train_data.ix[:, column].append(test_data.ix[:, column])
         label_encoder.fit(label_transform_data)
         train_data.ix[:, column] = label_encoder.transform(train_data.ix[:, column])
         test_data.ix[:, column] = label_encoder.transform(test_data.ix[:, column])
-        # print(label_encoder.classes_)
+
 
     train_data_last = train_data
     train_result_last = pd.DataFrame(train_result)
-    # show(train_data_after_label_transform, train_result_after_label_transform, "Age", "Fare")
+    print(train_data_last.head(10))
+    # show(train_data_last, train_result_last, "Embarked", "Fare")
 
-    train_data_last, train_result_last = remove_outliers(train_data_last, train_result_last)
+    # train_data_last, train_result_last = remove_outliers(train_data_last, train_result_last)
 
     min_samples_split = 2
 
     pre_feature_selection(train_data_last, train_result_last, min_samples_split)
-    feature_selection = SelectKBest(k=7)
-    train_data_last = feature_selection.fit_transform(train_data_last, train_result_last["Survived"])
-    test_data_after_transform = feature_selection.transform(test_data)
 
-    estimator = RandomForestClassifier(min_samples_split=min_samples_split)
-    # estimator = DecisionTreeClassifier()
-    # estimator = AdaBoostClassifier()
-    # estimator = GaussianNB()
-    # estimator = SVC(kernel="rbf")
-    # estimator = SVC(kernel="poly")
+    test_data_after_transform = test_data
+    # feature_selection = SelectKBest(k=7)
+    # train_data_last = feature_selection.fit_transform(train_data_last, train_result_last["Survived"])
+    # test_data_after_transform = feature_selection.transform(test_data)
+
+    from sklearn.linear_model import LogisticRegression
+    estimators = [ensemble.RandomForestClassifier(),
+                  DecisionTreeClassifier(),
+                  ensemble.AdaBoostClassifier(),
+                  GaussianNB(),
+                  SVC(),
+                  SGDClassifier(loss="hinge"),
+                  NearestCentroid(),
+                  ensemble.BaggingClassifier(KNeighborsClassifier(), max_samples=0.5, max_features=0.5),
+                  ensemble.GradientBoostingClassifier(),
+                  KNeighborsClassifier(3),
+                  ]
+
+    estimator_name = "RandomForestClassifier"
+    estimator = list(filter(lambda e: type(e).__name__ == estimator_name, estimators))[0]
 
     tuned_parameters = [{'min_samples_split': [2, 3, 4, 5, 6, 7, 8, 9]}]
-    test_estimator(estimator, train_data_last, train_result_last, tuned_parameters)
+    print("before estimate train data size:", train_data_last.shape)
+    cross_val_score_for_all_estimators(train_data_last, train_result_last, estimators)
+    # test_estimator(estimator, train_data_last, train_result_last, tuned_parameters, para_optimize=False)
 
+    print(train_data_last.head(10))
     estimator.fit(train_data_last, train_result_last["Survived"])
     train_predict = estimator.predict(train_data_last)
     print("")
@@ -107,16 +173,27 @@ def predict():
 
     test_predict = estimator.predict(test_data_after_transform)
 
-    df_result = pd.DataFrame(test_predict, columns=["Survived"])
-    df_result["PassengerId"] = test_data["PassengerId"]
+    df_result = pd.DataFrame(pd.read_csv("test.csv")["PassengerId"], columns=["PassengerId"])
+    df_result["Survived"] = test_predict
     df_result.reindex(["PassengerId", "Survived"])
+
+    print(df_result["Survived"].value_counts())
+    print(df_result["Survived"].value_counts(normalize=True))
     df_result.to_csv("result.csv", index=False)
 
 
-def test_estimator(estimator, train_data_last, train_result_last, tuned_parameters):
+def cross_val_score_for_all_estimators(train_data, train_result, estimators):
+    print("")
+    for estimator in estimators:
+        result = cross_validation.cross_val_score(estimator, train_data, train_result["Survived"], cv=5)
+        print(type(estimator).__name__, result.mean())
+
+def test_estimator(estimator, train_data_last, train_result_last, tuned_parameters, para_optimize):
     results = cross_validation.cross_val_score(estimator, train_data_last, train_result_last["Survived"], cv=5)
     print(results, results.mean())
 
+    if not para_optimize:
+        return
     from sklearn.model_selection import GridSearchCV
     clf = GridSearchCV(estimator, tuned_parameters, cv=5, scoring='precision_macro')
     clf.fit(train_data_last, train_result_last["Survived"])
@@ -145,7 +222,7 @@ def test_estimator(estimator, train_data_last, train_result_last, tuned_paramete
 
 
 def remove_outliers(train_data, train_result):
-    clf = IsolationForest()
+    clf = ensemble.IsolationForest()
     clf.fit(train_data)
     outlier = clf.predict(train_data)
     train_data["Outlier"] = outlier
@@ -160,7 +237,7 @@ def remove_outliers(train_data, train_result):
 
 
 def pre_feature_selection(train_data, train_result, min_samples_split):
-    forest = ExtraTreesClassifier(random_state=0, min_samples_split=min_samples_split)
+    forest = ensemble.ExtraTreesClassifier(random_state=0, min_samples_split=min_samples_split)
     forest.fit(train_data, train_result["Survived"])
     columns_weight = zip(train_data.columns, forest.feature_importances_)
     columns_weight = sorted(columns_weight, key=lambda x: x[1], reverse=True)
